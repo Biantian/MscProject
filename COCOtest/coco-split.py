e409456b57f4df254f421bb64b1417d37ae194ea
Biantian <jdzv587@foxmail.com>
2020-08-08 04:43:55 +0800

    finish coco-split.py

--- COCOtest/coco-split.py
+++ COCOtest/coco-split.py
@@ -1,10 +1,13 @@
 # Create annotation file for SubCOCO
+# Reference: https://github.com/mameng1/split-coco-datasets/blob/master/split_dastaset.py
 
 from pycocotools.coco import COCO
 import json
 import os
-dsNm = "val2017"
-catNms = ['person', 'car', 'bicycle']
+import shutil
+from tqdm import tqdm
+dsNm = "train2017"
+catNms = ['person', 'car']
 root = "E:/Resource/Dataset/COCO"  # addr of COCO
 dataDir = os.path.join(root, dsNm)
 annDir = os.path.join(root, "annotations/instances_{}.json").format(dsNm)
@@ -38,12 +41,17 @@ coco = COCO(annDir)
 
 # images
 catIds = coco.getCatIds(catNms=catNms)
-imgIds = coco.getImgIds(catIds=catIds)
+imgIds = []
+for catId in catIds:
+    imgIds = imgIds + coco.getImgIds(catIds=[catId])
+imgIds = list(dict.fromkeys(imgIds))  # remove duplicate id
+print("Quantity of images: ", len(imgIds))
 imgs = coco.loadImgs(imgIds)
 coco_output['images'] = imgs
 
 # annotations
 annIds = coco.getAnnIds(imgIds=imgIds, catIds=catIds, iscrowd=False)
+print("Quantity of masks: ", len(annIds))
 anns = coco.loadAnns(annIds)
 coco_output['annotations'] = anns
 
@@ -51,6 +59,21 @@ coco_output['annotations'] = anns
 cats = coco.loadCats(catIds)
 coco_output['categories'] = cats
 
-# save
-with open('{}/SubCOCO/annotation-little.json'.format(root), 'w') as output_json_file:
+# save annotations
+output_folder = '{}/SubCOCO/annotations'.format(root)
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+with open(os.path.join(output_folder, 'instances_{}.json'.format(dsNm)), 'w') as output_json_file:
     json.dump(coco_output, output_json_file)
+print("Annotation created!")
+
+# copy images
+input_folder = os.path.join(root, dsNm)
+output_folder = os.path.join(root, 'SubCOCO', dsNm)
+print('Copying images\nfrom: {}\nto: {}\n'.format(input_folder, output_folder))
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+for img in tqdm(imgs):
+    file_address = os.path.join(input_folder, img["file_name"])
+    shutil.copy(file_address, output_folder)
+print("\nImages copied!")
