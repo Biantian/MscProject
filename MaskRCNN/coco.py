1abeabf7bf9bb3959b5caecdc2be28dc06b0d104
Biantian <jdzv587@foxmail.com>
2020-08-13 21:35:46 +0800

    coco detection dataset object

--- MaskRCNN/coco.py
+++ MaskRCNN/coco.py
@@ -1,9 +1,5 @@
-import os
-import json
-import numpy as np
-import torch
-import torch.utils.data as data
-from torchvision import datasets, transforms
+from torchvision import datasets
+
 
 class CocoDetection(datasets.CocoDetection):
     """
@@ -13,10 +9,9 @@ class CocoDetection(datasets.CocoDetection):
     The target is a torch array with shape (c1 x h x w), channel c1 equals the number
     of objects in the image.
     """
-    def __init__(self, root, annFile, transform=None, target_transform=None, transforms=None):
-        super(CocoDetection, self).__init__(root, transforms, transform, target_transform)
-        from pycocotools.coco import COCO
-        self.coco = COCO(annFile)
+
+    def __init__(self, root, ann_file, transform=None, target_transform=None, transforms=None):
+        super(CocoDetection, self).__init__(root, ann_file, transform, target_transform, transforms)
 
         # transform categories
         self.category_ids = sorted(self.coco.cats.keys())
@@ -24,9 +19,39 @@ class CocoDetection(datasets.CocoDetection):
     def __getitem__(self, index):
         """
         Args:
+            index (int): Index
         Returns:
+            tuple: Tuple (image, target).
+                image is a torch array of the image.
+                target is multiple channel of the annotations.
+                Both are FloatTensor.
         """
+        img, target_ = super(CocoDetection, self).__getitem__(index)
 
+        # convert the annotations to mask
+        labels = []
+        masks = []
+        areas = []
+        boxes = []
+        iscrowd = []
+        image_id = target_[0]['image_id']
+        for ann in target_:
+            labels += ann['category_id']
+            masks += self.coco.annToMask(ann)
+            areas += ann['area']
+            boxes += ann['bbox']
+            iscrowd += ann['iscrowd']
 
-    def __len__(self):
-        return len()
+        # put target elements together
+        target = {"labels": labels,
+                  "masks": masks,
+                  "areas": areas,
+                  "boxes": boxes,
+                  "image_id": image_id,
+                  "iscrowd": iscrowd}
+
+        # this will handle all kinds of transforms
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
